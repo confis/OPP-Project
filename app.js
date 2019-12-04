a72a13e6bf7db392380e910fac671bf96e7f843c
Yaniv Cohen <confis@gmail.com>
2019-12-04 20:01:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+//let B = new Buttoned  ("foo", "mod", -100, 10, "green", 22, 33,12);
+//document.write(B.displayDetails() + '<img src="ITEMS/images/01.jpg" /> <hr>');
+//class Tester {
+//    test() {
+//        prompt("Please type the number of items you like to see","10")
+//    }
+//}
+//let T = new Tester()
+//T.test();
+//}
+function main() {
+    //let page = document.getElementById("c1");
+    var x = Number(prompt("Type the number of items you like to see", "10"));
+    //let arr: Array<items> = new Array<items>(x);
+    //let x1: Generator = new Generator(x);
+    //for (let i = 0; i < arr.length; i++) {
+    //    let pro: items = x1.getRandomItem();
+    //    console.log(pro);
+    //    arr[i] = pro;
+    //}
+    //for (let j = 0; j < arr.length; j++) {
+    //    page.innerHTML += `j=${j + 1}`;
+    //    arr[j].displayImage(page);
+    //    arr[j].displayDetails(page);
+    //    arr[j].getPriceWithVat(page);
+    //    arr[j].brand(page);
+    //    if (arr[j] instanceof Belt) {
+    //        page.innerHTML += "Buckle Color:" + (arr[j] as Belt).BuckleColor;;
+    //    }
+    //    page.innerHTML += `<hr/>`;
+    //}
+}
+//# sourceMappingURL=app.js.map
